b68f2d8101681dbfc31e9a5a26d550df752353f9
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-03-01 02:20:16 +0000

    Update action

--- src/actions_node/game_specific_actions/AutoBalanceAction.py
+++ src/actions_node/game_specific_actions/AutoBalanceAction.py
@@ -5,7 +5,7 @@ from frc_robot_utilities_py_node.frc_robot_utilities_py import *
 from actions_node.game_specific_actions.Subsystem import Subsystem
 from swerve_trajectory_node.srv import StopTrajectory, StopTrajectoryResponse
 from ck_ros_msgs_node.msg import Trajectory_Status, Swerve_Drivetrain_Auto_Control
-from ck_ros_base_msgs_node.msg import IMU_Data, IMU_Sensor_Data
+from nav_msgs.msg import Odometry
 from ck_utilities_py_node.geometry import *
 from enum import Enum
 from ck_utilities_py_node.pid_controller import PIDController
@@ -21,7 +21,7 @@ class AutoBalanceAction(Action):
     def __init__(self, balance_direction : BalanceDirection, balance_threshold_deg : float):
         self.__traj_status_subscriber = BufferedROSMsgHandlerPy(Trajectory_Status)
         self.__traj_status_subscriber.register_for_updates("/TrajectoryStatus")
-        self.__imu_subscriber = BufferedROSMsgHandlerPy(IMU_Data)
+        self.__imu_subscriber = BufferedROSMsgHandlerPy(Odometry)
         self.__imu_subscriber.register_for_updates("/RobotIMU")
         self.__balance_direction = balance_direction
         self.__balance_threshold = math.radians(balance_threshold_deg)
@@ -44,22 +44,21 @@ class AutoBalanceAction(Action):
             #Maybe check percentage complete of current traj before stopping it
             pass
 
-        imu_data : IMU_Data = self.__imu_subscriber.get()
+        imu_data : Odometry = self.__imu_subscriber.get()
         if imu_data is not None:
-            if len(imu_data.imuData) > 0:
-                imu_sensor_data : IMU_Sensor_Data = imu_data.imuData[0]
-                process_var : float = self.__determine_process_var(imu_sensor_data)
-            
-                control_msg : Swerve_Drivetrain_Auto_Control = Swerve_Drivetrain_Auto_Control()
+            imu_sensor_data : Pose = Pose(imu_data.pose.pose)
+            process_var : float = self.__determine_process_var(imu_sensor_data.orientation)
+        
+            control_msg : Swerve_Drivetrain_Auto_Control = Swerve_Drivetrain_Auto_Control()
 
-                if self.__balance_direction == BalanceDirection.PITCH:
-                    control_msg.twist.linear.x = self.__balance_pid.update(0, process_var)
-                    control_msg.twist.linear.y = 0
-                elif self.__balance_direction == BalanceDirection.ROLL:
-                    control_msg.twist.linear.x = 0
-                    control_msg.twist.linear.y = self.__balance_pid.update(0, process_var)
+            if self.__balance_direction == BalanceDirection.PITCH:
+                control_msg.twist.linear.x = self.__balance_pid.update(0, process_var)
+                control_msg.twist.linear.y = 0
+            elif self.__balance_direction == BalanceDirection.ROLL:
+                control_msg.twist.linear.x = 0
+                control_msg.twist.linear.y = self.__balance_pid.update(0, process_var)
 
-                self.__drive_twist_publisher.publish(control_msg)
+            self.__drive_twist_publisher.publish(control_msg)
 
 
 
@@ -68,12 +67,11 @@ class AutoBalanceAction(Action):
 
 
     def isFinished(self) -> bool:
-        imu_data : IMU_Data = self.__imu_subscriber.get()
+        imu_data : Odometry = self.__imu_subscriber.get()
         if imu_data is not None:
-            if len(imu_data.imuData) > 0:
-                imu_sensor_data : IMU_Sensor_Data = imu_data.imuData[0]
-                process_var : float = self.__determine_process_var(imu_sensor_data)
-                return within(process_var, 0, self.__balance_threshold)
+            imu_sensor_data : Pose = Pose(imu_data.pose.pose)
+            process_var : float = self.__determine_process_var(imu_sensor_data.orientation)
+            return within(process_var, 0, self.__balance_threshold)
         return False
 
 
@@ -81,7 +79,7 @@ class AutoBalanceAction(Action):
         return [ Subsystem.DRIVEBASE ]
     
 
-    def __determine_process_var(self, imu_sensor_data : IMU_Sensor_Data) -> float:
+    def __determine_process_var(self, imu_sensor_data : Rotation) -> float:
         process_var : float = 0
         if self.__balance_direction == BalanceDirection.PITCH:
             process_var = imu_sensor_data.pitch
