b46536cf84b349dc3c2808aa5a9177cbe3ebe109
Tom <tslourenco@wpi.edu>
2023-03-06 04:39:39 +0000

    Latest iteration of the auto balance.

--- src/actions_node/game_specific_actions/AutoBalanceAction.py
+++ src/actions_node/game_specific_actions/AutoBalanceAction.py
@@ -46,18 +46,15 @@ class AutoBalanceAction(Action):
     An action that commands a twist to the swerve drivetrain to balance on the charge station.
     """
 
-    def __init__(self, balance_direction: BalanceDirection):
+    def __init__(self, balance_direction: BalanceDirection, desired_heading: int = 0):
         rospy.logdebug("Init Auto-Balance Action")
 
         self.__imu_subscriber = BufferedROSMsgHandlerPy(Odometry)
         self.__imu_subscriber.register_for_updates("/RobotIMU")
 
-        self.__odometry_subscriber = BufferedROSMsgHandlerPy(Odometry)
-        self.__odometry_subscriber.register_for_updates("/odometry/filtered")
-
         self.__balance_direction = balance_direction
-        self.__desired_heading = 0
-        self.__pitch_rate_average = MovingAverage(10)
+        self.__desired_heading = desired_heading
+        self.__attitude_rate_moving_average = MovingAverage(3)
         alliance = robot_status.get_alliance()
         self.__alliance = alliance
 
@@ -67,7 +64,11 @@ class AutoBalanceAction(Action):
         self.__drive_twist_publisher = rospy.Publisher(name="/SwerveAutoControl", data_class=Swerve_Drivetrain_Auto_Control, queue_size=10, tcp_nodelay=True)
         self.__start_time = 0
         self.__level_time = 0
+
         self.__imu_pose = Pose()
+        self.__imu_twist = Twist()
+
+        self.__tipped = 0
 
     def start(self):
         rospy.logdebug("Starting Auto-Balance Action")
@@ -76,6 +77,8 @@ class AutoBalanceAction(Action):
         stop_traj = rospy.ServiceProxy('/stop_trajectory', StopTrajectory)
         stop_traj_response: StopTrajectoryResponse = stop_traj()
 
+        self.__tipped = 0
+
         self.__start_time = rospy.Time.now().to_sec()
 
         if not stop_traj_response.accepted:
@@ -83,79 +86,77 @@ class AutoBalanceAction(Action):
 
     def update(self):
 
-        filtered_odometry_data: Odometry = self.__odometry_subscriber.get()
-
-        if filtered_odometry_data is not None:
-            self.__current_pose = Pose(filtered_odometry_data.pose.pose)
+        rospy.logerr(f"Tipped: {self.__tipped}")
 
         imu_data: Odometry = self.__imu_subscriber.get()
 
         if imu_data is not None:
-            current_twist: Twist = Twist(imu_data.twist.twist)
             self.__imu_pose: Pose = Pose(imu_data.pose.pose)
+            self.__imu_twist: Twist = Twist(imu_data.twist.twist)
 
             control_msg: Swerve_Drivetrain_Auto_Control = Swerve_Drivetrain_Auto_Control()
             control_msg.pose.orientation = self.__desired_quat
-            control_msg.pose.position.x = 11
+
+            control_msg.twist = self.__calculate_twist(False).to_msg()
+
+            attitude = 0.0
+            attitude_rate = 0.0
 
             if self.__balance_direction == BalanceDirection.PITCH:
-                rospy.logerr(f"Pitch: {degrees(self.__imu_pose.orientation.pitch)} Roll: {degrees(self.__imu_pose.orientation.roll)} PitchRate: {current_twist.angular.pitch}")
+                attitude = self.__imu_pose.orientation.pitch
+                attitude_rate = self.__imu_twist.angular.pitch
+            elif self.__balance_direction == BalanceDirection.ROLL:
+                attitude = self.__imu_pose.orientation.roll
+                attitude_rate = self.__imu_twist.angular.roll
 
-                control_msg.twist = self.__calculate_twist(False).to_msg()
+            # rospy.logerr(f"Pitch: {degrees(self.__imu_pose.orientation.pitch)} Roll: {degrees(self.__imu_pose.orientation.roll)}")
+            # rospy.logerr(f"PitchRate: {degrees(self.__imu_twist.angular.pitch)},  RollRate: {degrees(self.__imu_twist.angular.roll)}")
 
-                max_pitch_error = 0.60
+            # self.__attitude_rate_moving_average.add_sample(limit(abs(attitude_rate) - 0.1, 0.0, 2.0))
 
+            KP = 0.005
+            BASE_MAX_SPEED = 0.5
+            # MOVING_AVERAGE_GAIN = 2.0
+            # max_speed = limit(BASE_MAX_SPEED - self.__attitude_rate_moving_average.get_average() * MOVING_AVERAGE_GAIN, 0, BASE_MAX_SPEED)
 
-                sign = np.sign(self.__imu_pose.orientation.pitch)
-                pitch_error = degrees(self.__imu_pose.orientation.pitch)
-                pitch_error = pitch_error * pitch_error
-                pitch_error = pitch_error * 0.0060 * sign
+            output = np.sign(attitude) * degrees(attitude) * degrees(attitude) * KP
+            output = limit(output, -BASE_MAX_SPEED, BASE_MAX_SPEED)
 
-                pitch_error = limit(pitch_error, -max_pitch_error, max_pitch_error)
+            control_msg.twist.linear.x = control_msg.twist.linear.x * output
+            control_msg.twist.linear.y = control_msg.twist.linear.y * output
 
-                control_msg.twist.linear.x = control_msg.twist.linear.x * pitch_error
-                control_msg.twist.linear.y = control_msg.twist.linear.y * pitch_error
-
+            if abs(attitude_rate) > 0.30:
+                self.__tipped += 1
+                if self.__balance_direction == BalanceDirection.PITCH:
+                    control_msg.twist.linear.x = 0.0
+                    control_msg.twist.linear.y = 0.2
+                elif self.__balance_direction == BalanceDirection.ROLL:
+                    control_msg.twist.linear.x = 0.2
+                    control_msg.twist.linear.y = 0.0
             else:
-                rospy.logerr(f"Pitch: {degrees(self.__imu_pose.orientation.pitch)} Roll: {degrees(self.__imu_pose.orientation.roll)} PitchRate: {current_twist.angular.pitch}")
-
-                control_msg.twist = self.__calculate_twist(False).to_msg()
-
-                max_roll_error = 0.60
-
-
-                sign = np.sign(self.__imu_pose.orientation.roll)
-                roll_error = degrees(self.__imu_pose.orientation.roll)
-                roll_error = roll_error * roll_error
-                roll_error = roll_error * 0.0060 * sign
-
-                roll_error = limit(roll_error, -max_roll_error, max_roll_error)
-
-                control_msg.twist.linear.x = control_msg.twist.linear.x * roll_error
-                control_msg.twist.linear.y = control_msg.twist.linear.y * roll_error
-
-
-            if abs(current_twist.angular.pitch) > 0.15 or abs(current_twist.angular.roll) > 0.15:
-                control_msg.twist.linear.x = 0
-                control_msg.twist.linear.y = 0
+                self.__tipped = 0
 
             self.__drive_twist_publisher.publish(control_msg)
 
     def done(self):
         control_msg: Swerve_Drivetrain_Auto_Control = Swerve_Drivetrain_Auto_Control()
         control_msg.pose.orientation = self.__desired_quat
+        control_msg.twist.linear.x = 0.0
+        control_msg.twist.linear.y = 0.0
         self.__drive_twist_publisher.publish(control_msg)
 
     def isFinished(self) -> bool:
-        offset = abs(self.__imu_pose.orientation.pitch) + abs(self.__imu_pose.orientation.roll)
-        offset = degrees(offset)
-        if offset < 5:
-            self.__level_time = rospy.Time.now().to_sec()
-            rospy.logerr(f"Level atm: {offset}")
-        else:
-            self.__start_time = rospy.Time.now().to_sec()
-            rospy.logerr(f"Resetting the counter: {offset}")
-        return self.__level_time > self.__start_time + 1
+        # offset = abs(self.__imu_pose.orientation.pitch) + abs(self.__imu_pose.orientation.roll)
+        # offset = degrees(offset)
+        # if offset < 5:
+        #     self.__level_time = rospy.Time.now().to_sec()
+        #     rospy.logerr(f"Level atm: {offset}")
+        # else:
+        #     self.__start_time = rospy.Time.now().to_sec()
+        #     rospy.logerr(f"Resetting the counter: {offset}")
+        # return self.__level_time > self.__start_time + 1
+        return self.__tipped > 1
+
 
     def affectedSystems(self) -> List[Subsystem]:
         return [Subsystem.DRIVEBASE]
