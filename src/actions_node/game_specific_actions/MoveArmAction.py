47337ce47dce0c023b27262473f5cba155c9b967
Tom <TSLourenco@WPI.edu>
2023-03-25 20:47:36 +0000

    Updated move arm action.

--- src/actions_node/game_specific_actions/MoveArmAction.py
+++ src/actions_node/game_specific_actions/MoveArmAction.py
@@ -7,49 +7,67 @@ from typing import List
 from actions_node.game_specific_actions.Subsystem import Subsystem
 from actions_node.game_specific_actions import constant
 
+
 class MoveArmAction(Action):
+    """
+    Action to control the arm and wrist in autonomous.
+    """
     arm_subsystem = SubsystemController[Arm_Goal, Arm_Status]('ArmGoal', Arm_Goal, 'ArmStatus', Arm_Status)
 
-    def __init__(self, arm_goal : int, arm_goal_side : int, wrist_goal : int = 0, base_deviation_tolerance_deg : int = -1, upper_deviation_tolerance_deg : int = -1):
+    def __init__(self, arm_goal: int, arm_goal_side: int, base_deviation_tolerance_deg: int = -1, upper_deviation_tolerance_deg: int = -1, wrist_deviation_tolerance_deg: int = -1):
         self.__Arm_Goal_msg = Arm_Goal()
         self.__Arm_Goal_msg.goal = arm_goal
         self.__Arm_Goal_msg.goal_side = arm_goal_side
-        self.__Arm_Goal_msg.wrist_goal = wrist_goal
         self.__base_deviation_tolerance_deg = base_deviation_tolerance_deg
         self.__upper_deviation_tolerance_deg = upper_deviation_tolerance_deg
+        self.__wrist_deviation_tolerance_deg = wrist_deviation_tolerance_deg
 
-    #Do not call these methods directly
+    # Do not call these methods directly
     def start(self):
         self.arm_subsystem.publish(self.__Arm_Goal_msg)
 
-    #Do not call these methods directly
+    # Do not call these methods directly
     def update(self):
         pass
 
-    #Do not call these methods directly
+    # Do not call these methods directly
     def done(self):
         pass
 
-    #Do not call these methods directly
     def isFinished(self) -> bool:
-        arm_status : Arm_Status = self.arm_subsystem.get()
+        """
+        Reports if the state is finished, to allow for transition.
+        Do not call this method directly.
+        """
+        arm_status: Arm_Status = self.arm_subsystem.get()
+
         if arm_status is None:
-            rospy.logerr("No status update present from arm")
+            rospy.logerr("No status update present from arm.")
             return False
-        
-        is_correct_arm_goal = arm_status.goal.goal == self.__Arm_Goal_msg.goal
 
-        if (self.__base_deviation_tolerance_deg <= 0 and self.__upper_deviation_tolerance_deg <= 0):
-            return is_correct_arm_goal and arm_status.arm_at_setpoint
-        elif (self.__base_deviation_tolerance_deg <= 0 and self.__upper_deviation_tolerance_deg > 0):
-            return is_correct_arm_goal and abs(arm_status.arm_upper_deviation_deg) < self.__upper_deviation_tolerance_deg
-        elif (self.__base_deviation_tolerance_deg > 0 and self.__upper_deviation_tolerance_deg <= 0):
-            return is_correct_arm_goal and abs(arm_status.arm_base_deviation_deg) < self.__base_deviation_tolerance_deg
-        elif (self.__base_deviation_tolerance_deg > 0 and self.__upper_deviation_tolerance_deg > 0):
-            return is_correct_arm_goal and abs(arm_status.arm_base_deviation_deg) < self.__base_deviation_tolerance_deg \
-                and abs(arm_status.arm_upper_deviation_deg) < self.__upper_deviation_tolerance_deg
+        # If not at the correct arm goal, then the movement is not finished.
+        if arm_status.goal.goal != self.__Arm_Goal_msg.goal:
+            return False
 
+        # If all the tolerances are negative, use the "at setpoint" status.
+        if self.__base_deviation_tolerance_deg <= 0 and self.__upper_deviation_tolerance_deg <= 0 and self.__wrist_deviation_tolerance_deg <= 0:
+            return arm_status.arm_at_setpoint
 
-    #Do not call these methods directly
+        # Determine whether the arm is within the tolerances provided.
+        base_arm_at_setpoint = True
+        if self.__base_deviation_tolerance_deg > 0:
+            base_arm_at_setpoint = abs(arm_status.arm_base_deviation_deg) < self.__base_deviation_tolerance_deg
+
+        upper_arm_at_setpoint = True
+        if self.__upper_deviation_tolerance_deg > 0:
+            upper_arm_at_setpoint = abs(arm_status.arm_upper_deviation_deg) < self.__upper_deviation_tolerance_deg
+
+        wrist_arm_at_setpoint = True
+        if self.__wrist_deviation_tolerance_deg > 0:
+            wrist_arm_at_setpoint = abs(arm_status.arm_wrist_deviation_deg) < self.__wrist_deviation_tolerance_deg
+
+        return base_arm_at_setpoint and upper_arm_at_setpoint and wrist_arm_at_setpoint
+
+    # Do not call these methods directly
     def affectedSystems(self) -> List[Subsystem]:
-        return [ Subsystem.ARM ]
+        return [Subsystem.ARM]
